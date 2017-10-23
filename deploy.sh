ce658f6e097233556fec3360f6d81760a8a24681
root <root@v45886.1blu.de>
2017-10-24 01:50:37 +0200

    Build fixes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-export WWW=/var/www/vhost/shortbox.de
+export WWW=/var/www/vhosts/shortbox.de
 kill -9 `cat $WWW/server_pid`
 rm -f $WWW/server
 cd shortbox-backend/dist
 tar -xvf shortbox_backend.tar -C $WWW
-cd ..
+cd ../..
 cd shortbox-web/dist
 rm -f $WWW/httpdocs/*.js
 rm -f $WWW/httpdocs/*.css
 rm -f $WWW/httpdocs/*.html
 rm -f $WWW/httpdocs/*.map
 tar -xvf shortbox_web.tar -C $WWW/httpdocs
-cd ..
+cd ../..
 cp ./startup.sh $WWW/startup.sh
